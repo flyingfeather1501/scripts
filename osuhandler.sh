7514dedab60ad02d8a4a65342ce9340a17ffb0b5
Kisaragi Hiu <flyingfeather1501@gmail.com>
2017-06-14 23:26:19 +0800

    Remove osuhandler.sh. /usr/bin/osu does everything already, just need to pass uri to it

--- osuhandler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# pass all arguments to osu!.exe
-export WINEPREFIX=$HOME/.local/share/wineprefixes/osu/
-export OSUDIR=$HOME/.local/share/wineprefixes/osu/drive_c/users/flyin1501/Local\ Settings/Application\ Data/osu!/
-wine $OSUDIR/osu\!.exe $@
